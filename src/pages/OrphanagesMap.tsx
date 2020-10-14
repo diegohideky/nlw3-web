585dd3b1c31bdd16cb3999f8c8fd49ac4eb6b932
Diego Hideky <diegohideky@gmail.com>
2020-10-14 16:51:05 -0300

    Created web

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
+import '../styles/pages/orphanage-map.css';
+
+import mapMarkerImg from '../images/map-maker.svg';
+
+function OrphanagesMap() {
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="Happy"/>
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Rio Grande do Sul</strong>
+          <span>Santa Catarina</span>
+        </footer>
+      </aside>
+
+      <Map
+        center={[-23.4305731, -46.4752317]}
+        zoom={15}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      </Map>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
+  );
+}
+
+export default OrphanagesMap;
